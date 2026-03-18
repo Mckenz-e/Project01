c9574f8d06f6a4a7c5a87a7882ca5369b5fa20d9
Mckenzie <attapol717@gmail.com>
2026-03-18 18:24:49 +0700

    Delete project-re-2 directory

--- project-re-2/Backend/config/constants.js
+++
@@ -1,8 +0,0 @@
-const PORT = process.env.PORT || 8000;
-
-const STATUS = {
-  AVAILABLE: 1,
-  BORROWED: 0,
-};
-
-module.exports = { PORT, STATUS };
--- project-re-2/Backend/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = (req, res, next) => {
-  const now = new Date().toISOString();
-  console.log(`[${now}] ${req.method} ${req.url}`);
-  next();
-};
-
-module.exports = logger;
